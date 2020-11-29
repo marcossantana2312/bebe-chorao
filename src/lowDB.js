6a986799525035ddde4a063da41b64ce9a038993
Marcos da Silva Santana <marcos.santana@cubos.io>
2020-11-29 20:09:29 -0300

    firs blood

---
+++ src/lowDB.js
@@ -0,0 +1,8 @@
+const lowdb = require('lowdb');
+const fs = require('lowdb/adapters/FileSync');
+
+const adapter = new fs('db.json')
+
+const db = lowdb(adapter);
+
+module.exports = db;
---
+++ src/server.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const bodyParser = require("body-parser");
+const db = require("./lowDB");
+
+const app = express()
+
+const sonoInterval = [0, 400];
+const fomeInterval = [401, 600];
+const colicaInterval = [601, 800];
+const dorInterval = [801, 1000]
+
+const PORT = 3000;
+
+function getRandomValue(min = 1, max = 10) {
+    return Math.floor(Math.random() * (max - min + 1)) + 1;
+}
+
+app.use(bodyParser.json());
+
+db.defaults({ "choroOcorrencias": { "sono": [], "colica": [], "fome": [], "dor": [] } })
+    .write();
+
+app.post("/", async (req, res) => {
+    const { data } = req.body;
+
+    await Promise.all(data.map(value => {
+        const now = new Date();
+
+        if (value >= sonoInterval[0] && value <= sonoInterval[1]) {
+            return db
+                .get("choroOcorrencias")
+                .get("sono")
+                .push(now)
+                .write();
+        } else if (value >= fomeInterval[0] && value <= fomeInterval[1], now) {
+            return db
+                .get("choroOcorrencias")
+                .get("colica")
+                .push(now)
+                .write();
+        } else if (value >= colicaInterval[0] && value <= colicaInterval[1], now) {
+            return db
+                .get("choroOcorrencias")
+                .get("fome")
+                .push(now)
+                .write();
+            } else if (value >= dorInterval[0] && value <= dorInterval[1]) {
+            return db
+                .get("choroOcorrencias")
+                .get("dor")
+                .push(now)
+                .write();
+        }
+    }))
+    res.sendStatus(200);
+
+})
+
+app.get('/', async (req, res) => {
+
+    const dbResponse = db.get("choroOcorrencias").value();
+    const { inicio, fim } = req.params;
+    console.log(req.params)
+
+    const ret = {
+        colica: dbResponse["colica"].filter(date => inicio && fim ? date >= inicio && date <= fim :  true).length,
+        fome: dbResponse["fome"].filter(date => inicio && fim ? date >= inicio && date <= fim :  true).length,
+        sono: dbResponse["sono"].filter(date => inicio && fim ? date >= inicio && date <= fim :  true).length,
+        dor: dbResponse["dor"].filter(date => inicio && fim ? date >= inicio && date <= fim :  true).length
+    }
+    res.send(ret);
+})
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta http://localhost:${PORT}`))
