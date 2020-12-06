7d0f5399360e6639854bc588f30dbf00a3103b34
Giovanni <giovannilima262@gmail.com>
2020-12-06 15:20:19 -0300

    finalização do login

--- te_arduino/lib/main.dart
+++ te_arduino/lib/main.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:te_arduino/login.dart';
 
 import 'bar_chart/bar_chart_page2.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor:
+          SystemUiOverlayStyle.dark.systemNavigationBarColor,
+    ),
+  );
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'BB',
-      showPerformanceOverlay: false,
+      title: 'Login Demo',
       theme: ThemeData(
-        primaryColor: const Color(0xff262545),
-        primaryColorDark: const Color(0xff201f39),
-        brightness: Brightness.dark,
+        // brightness: Brightness.dark,
+        primarySwatch: Colors.deepPurple,
+        accentColor: Colors.orange,
+        cursorColor: Colors.orange,
+        // fontFamily: 'SourceSansPro',
+        textTheme: TextTheme(
+          button: TextStyle(
+            // OpenSans is similar to NotoSans but the uppercases look a bit better IMO
+            fontFamily: 'OpenSans',
+          ),
+          caption: TextStyle(
+            fontFamily: 'NotoSans',
+            fontSize: 12.0,
+            fontWeight: FontWeight.normal,
+            color: Colors.deepPurple[300],
+          ),
+          overline: TextStyle(fontFamily: 'NotoSans'),
+        ),
       ),
-      home: const MyHomePage(title: 'fl_chart'),
+      home: LoginScreen(),
     );
   }
 }
